6a01e104fe4f4fbf670415b946f4c69087b0cd1d
sonirishav56-lab <sonirishav56@gmail.com>
2026-01-12 20:29:45 +0530

    jjjj

--- basic-programs/helloworld11.java
+++ basic-programs/helloworld11.java
@@ -1,7 +1,8 @@
 public class helloworld11 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+         System.out.println("Hello World rishub");
+
 		 
     }
 }
